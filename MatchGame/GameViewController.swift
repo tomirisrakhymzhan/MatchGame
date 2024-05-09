18de9c509ad13c25a75badc7188bf3a44fd25e68
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2024-05-09 13:12:03 +0500

    refactored logic to remove bugs

--- MatchGame/GameViewController.swift
+++ MatchGame/GameViewController.swift
@@ -15,98 +15,61 @@ class GameViewController: UIViewController {
     
     @IBOutlet weak var mistakesLabel: UILabel!
     
-    @IBOutlet weak var cardButton: UIButton!
-    
-    
     @IBOutlet weak var playBtn: UIButton!
     
     var timer : Timer?
     var score = 0
-    var countSeconds = 0
+    var secondsElapsed = 0
     var countMoves = 0
     var countMistakes = 0
     var isPlaying = false
-    var carToCompare : (Int, String) = (0, "")
+    var previousCard : (Int, String)? = (0, "")
     var states = Dictionary<Int, String>()
     
     let formulaOneCars = ["ferrari", "mercedes", "redbull", "maclaren", "alpine", "astonmartin", "haas", "sauber"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
     }
-    
-    
-    @IBAction func cardPressed(_ sender: UIButton) {
-        if isPlaying == true {
-            let tag = sender.tag
-            let openedCar : (Int, String) = (tag, states[tag]!)
-            
-            sender.setBackgroundImage(UIImage(named: openedCar.1), for: .normal)
-    
-            countMoves += 1
-            if countMoves % 2 == 0 {
-                //check pair
-                if carToCompare.1 == openedCar.1 {
-                    score += 1
-                    //disable opened cards
-                    if let curButton = self.cardButton.viewWithTag(carToCompare.0) as? UIButton{
-                        curButton.isEnabled = false
-                        sender.isEnabled = false
-                    }
-                } else {
-                    countMistakes += 1
-                    if let curButton = view.viewWithTag(carToCompare.0) as? UIButton{
-                        curButton.setBackgroundImage(UIImage(named: "blue"), for: .normal)
-                        sender.setBackgroundImage(UIImage(named: "blue"), for: .normal)
-                    }
-                    
-                }
-            } else {
-                // save openedCar for future move check
-                carToCompare = openedCar
-                
-            }
-        }
-        
-    }
-    
     @IBAction func playPressed(_ sender: UIButton) {
-        sender.isEnabled = false
-        sender.isHidden = true
-        score = 0
-        countSeconds = 0
-        countMoves = 0
-        countMistakes = 0
-        isPlaying = true
-        //init the grid
-        initializeGame()
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { _ in
-            self.countSeconds += 1
-            self.timerLabel.text = "\(self.countSeconds) sec"
-            self.movesLabel.text = "Moves: \(self.countMoves)"
-            self.mistakesLabel.text = "Mistakes: \(self.countMistakes)"
-
-
-            print("Timer fired \(self.countSeconds)")
-        })
+        startGame()
     }
     
+    func startGame() {
+        resetGame()
+        setupNewGame()
+        isPlaying = true
+        playBtn.isHidden = true
+        
+        // Start timer
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    @objc func updateTimer() {
+        secondsElapsed += 1
+        timerLabel.text = "\(secondsElapsed) sec"
+    }
+    func setupNewGame() {
+        generateRandomCards()
+        enableAllButtons()
+        turnAllCardsBlue()
+    }
     
-    @IBAction func resetPressed(_ sender: Any) {
-        playBtn.isEnabled = true
-        playBtn.isHidden = false
+    func resetGame() {
         stopTimer()
-        score = 0
-        countSeconds = 0
+        secondsElapsed = 0
         countMoves = 0
         countMistakes = 0
-        isPlaying = false
-        turnGridToAllBlue()
-        disableAllGridButtons()
-        print("Reset game")
+        previousCard = nil
+        states = [:]
+        
+        playBtn.isHidden = false
+        
+        // Reset UI elements
+        timerLabel.text = "0 sec"
+        movesLabel.text = "Moves: 0"
+        mistakesLabel.text = "Mistakes: 0"
+        turnAllCardsBlue()
+        disableAllButtons()
     }
     
     func stopTimer(){
@@ -116,30 +79,91 @@ class GameViewController: UIViewController {
     deinit {
         stopTimer()
     }
-    func generateRandomGridStates() {
-        var carPool = [String]()
-        for car in formulaOneCars{
-            carPool.append(car)
-            carPool.append(car)
+    
+    
+    @IBAction func cardPressed(_ sender: UIButton) {
+        guard isPlaying else { return }
+        let cardIndex = sender.tag
+        guard let cardImage = states[cardIndex] else { return }
+             
+        sender.setBackgroundImage(UIImage(named: cardImage), for: .normal)
+
+        if let previous = previousCard {
+            countMoves += 1
+            movesLabel.text = "Moves: \(countMoves)"
+            
+            if previous.1 == cardImage {
+                // Cards match
+                disableButton(withTag: previous.0)
+                disableButton(withTag: cardIndex)
+                previousCard = nil
+            } else {
+                // No match, increment mistakes count
+                countMistakes += 1
+                mistakesLabel.text = "Mistakes: \(countMistakes)"
+                
+                // Delay to flip the cards back
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    sender.setBackgroundImage(UIImage(named: "blue"), for: .normal)
+                    self.flipButton(withTag: previous.0)
+                    self.previousCard = nil
+                }
+            }
+        } else {
+            // Store the previous card for future comparison
+            previousCard = (cardIndex, cardImage)
         }
-        carPool.shuffle()
         
-        var carPoolIndex = 0
-        for i in 0...15 {
-            states[i] = carPool[carPoolIndex]
-            carPoolIndex += 1
+    }
+    
+    func turnAllCardsBlue() {
+        for index in 0..<16 {
+            flipButton(withTag: index)
         }
     }
     
-    func initializeGame(){
-        if isPlaying == false {
-            turnGridToAllBlue()
-            disableAllGridButtons()
-        } else if isPlaying == true {
-            generateRandomGridStates()
-            enableAllGridButtons()
-            turnGridToAllBlue()
+    func flipButton(withTag tag: Int) {
+        if let button = view.viewWithTag(tag) as? UIButton {
+            button.setBackgroundImage(UIImage(named: "blue"), for: .normal)
         }
+    }
+    
+    func disableAllButtons() {
+        setAllButtonsEnabled(false)
+    }
+    
+    func enableAllButtons() {
+        setAllButtonsEnabled(true)
+    }
+    
+    func setAllButtonsEnabled(_ isEnabled: Bool) {
+        for index in 0..<16 {
+            if let button = view.viewWithTag(index) as? UIButton {
+                button.isEnabled = isEnabled
+            }
+        }
+    }
+    
+    func disableButton(withTag tag: Int) {
+        if let button = view.viewWithTag(tag) as? UIButton {
+            button.isEnabled = false
+        }
+    }
+    
+    @IBAction func resetPressed(_ sender: Any) {
+        resetGame()
+    }
+    
+    func generateRandomCards() {
+        // Create a pool of images and shuffle it
+        var cardPool = formulaOneCars + formulaOneCars
+        cardPool.shuffle()
+        
+        // Assign images to card indices
+        for index in 0..<16 {
+            states[index] = cardPool[index]
+        }
+        print(states)
     }
     
     func turnGridToAllBlue() {
@@ -151,21 +175,17 @@ class GameViewController: UIViewController {
     }
     
     func disableAllGridButtons(){
-        var tag = 0
-        for _ in 0...16{
+        for tag in 0...15{
             if let button = view.viewWithTag(tag) as? UIButton {
                 button.isEnabled = false
-                tag += 1
             }
         }
     }
     
     func enableAllGridButtons(){
-        var tag = 0
-        for _ in 0...16{
+        for tag in 0...15{
             if let button = view.viewWithTag(tag) as? UIButton {
                 button.isEnabled = true
-                tag += 1
             }
         }
     }
